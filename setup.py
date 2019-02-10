b0b8a25be1d83e7f4eaea326de1add837ad29d91
fkrieter <46888282+fkrieter@users.noreply.github.com>
2019-02-10 22:10:20 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="mephisto",
+    version=0.1,
+    author="fkrieter",
+    author_email="",
+    description="MakE Pretty HISTOgrams",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/fkrieter/mephisto",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+    scripts=[],
+    install_requires=["root_numpy"],
+)
