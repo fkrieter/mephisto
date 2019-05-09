4159d43354145e1de849b7e85870f561562ae022
fkrieter <46888282+fkrieter@users.noreply.github.com>
2019-05-09 18:08:31 +0200

    kind of fixed the Text size getters scaling with pad size

--- mephisto/Text.py
+++ mephisto/Text.py
@@ -17,12 +17,85 @@ class Text(MethodProxy, ROOT.TLatex):
         kwargs.setdefault("template", "common")
         self.DeclareProperties(**kwargs)
 
-    def GetXsize(self, ndc=False):
+    def GetXsize(self, **kwargs):
+        ndc = kwargs.pop("ndc", False)
+        scale = kwargs.pop("scale", 1.0)
+        if kwargs:
+            logger.error(
+                "Unknown key word argument(s): '{}'".format(
+                    "', '".join(kwargs.values())
+                )
+            )
         self.Draw()
         norm = ROOT.gPad.GetWw() if ndc else 1.0
-        return super(Text, self).GetXsize() * 0.95 * self.GetTextSize() / (16.0 * norm)
+        norm *= scale ** 0.7  # wild exponent appeared...
+        return super(Text, self).GetXsize() * self.GetTextSize() / (16.0 * norm)
 
-    def GetYsize(self, ndc=False):
+    def GetYsize(self, **kwargs):
+        ndc = kwargs.pop("ndc", False)
+        scale = kwargs.pop("scale", 1.0)
+        if kwargs:
+            logger.error(
+                "Unknown key word argument(s): '{}'".format(
+                    "', '".join(kwargs.values())
+                )
+            )
         self.Draw()
         norm = ROOT.gPad.GetWh() if ndc else 1.0
-        return super(Text, self).GetYsize() * 0.95 * self.GetTextSize() / (16.0 * norm)
+        norm *= scale ** 0.7  # wild exponent appeared...
+        return super(Text, self).GetYsize() * self.GetTextSize() / (16.0 * norm)
+
+
+if __name__ == "__main__":
+
+    from Plot import Plot
+
+    p1 = Plot()
+    p2 = Plot(npads=2)
+    p3 = Plot(npads=3)
+
+    x_0 = 0.5
+    y_0 = 0.5
+
+    print("PLOT 1 (npads = 1)")
+    t1 = Text(x_0, y_0, "TEST 12345")
+    print(t1.GetY())
+    t2 = Text(x_0, t1.GetY() - t1.GetYsize(scale=p1.GetPadHeight(0)), "TEST 12345")
+    print(t2.GetY())
+    t3 = Text(x_0, t2.GetY() - t2.GetYsize(scale=p1.GetPadHeight(0)), "TEST 12345")
+    print(t3.GetY())
+    t4 = Text(
+        x_0 + t1.GetXsize(scale=p1.GetPadWidth(0)),
+        t1.GetY() - t2.GetYsize(scale=p1.GetPadHeight(0)),
+        "TEST 12345",
+    )
+
+    p1.Register(t1)
+    p1.Register(t2)
+    p1.Register(t3)
+    p1.Register(t4)
+    p1.Print("text_test-1.pdf")
+
+    print("PLOT 2 (npads = 2)")
+    print(t1.GetY())
+    t2 = Text(x_0, t1.GetY() - t1.GetYsize(scale=p2.GetPadHeight(0)), "TEST 12345")
+    print(t2.GetY())
+    t3 = Text(x_0, t2.GetY() - t2.GetYsize(scale=p2.GetPadHeight(0)), "TEST 12345")
+    print(t3.GetY())
+
+    p2.Register(t1, pad=0)
+    p2.Register(t2, pad=0)
+    p2.Register(t3, pad=0)
+    p2.Print("text_test-2.pdf")
+
+    print("PLOT 2 (npads = 3)")
+    print(t1.GetY())
+    t2 = Text(x_0, t1.GetY() - t1.GetYsize(scale=p3.GetPadHeight(0)), "TEST 12345")
+    print(t2.GetY())
+    t3 = Text(x_0, t2.GetY() - t2.GetYsize(scale=p3.GetPadHeight(0)), "TEST 12345")
+    print(t3.GetY())
+
+    p3.Register(t1, pad=0)
+    p3.Register(t2, pad=0)
+    p3.Register(t3, pad=0)
+    p3.Print("text_test-3.pdf")
