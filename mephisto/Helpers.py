d82f68f6b990b68ffd471468bb5560750d4687df
fkrieter <46888282+fkrieter@users.noreply.github.com>
2019-05-17 11:30:57 +0200

    squelch the run time logging

--- mephisto/Helpers.py
+++ mephisto/Helpers.py
@@ -180,7 +180,7 @@ def timeit(method):
             name = kw.get("log_name", method.__name__.upper())
             kw["log_time"][name] = int((te - ts) * 1000)
         else:
-            logger.info(
+            logger.debug(
                 "Executed {} in {:.2f} ms".format(method.__name__, (te - ts) * 1000)
             )
         return result
