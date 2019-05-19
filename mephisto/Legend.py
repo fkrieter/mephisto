a30f6b748bb67756eea2d90c3e2f120cc7237f4f
fkrieter <46888282+fkrieter@users.noreply.github.com>
2019-05-19 15:14:25 +0200

    decouple Legend scaling from Canvas dimensions

--- mephisto/Legend.py
+++ mephisto/Legend.py
@@ -50,6 +50,19 @@ class Legend(MethodProxy, ROOT.TLegend):
     def Draw(self, option="", **kwargs):
         for histo in self._store:
             self.AddEntry(histo, histo.GetTitle(), histo.GetLegendDrawOption())
+        self.BuildFrame()
+        # Scaling (keep Legend frame independent of pad/canvas aspect ratio)
+        cpad = ROOT.gPad  # current pad
+        canvasheight = cpad.GetCanvas().GetWh() * cpad.GetCanvas().GetAbsHNDC()
+        canvaswidth = cpad.GetCanvas().GetWw() * cpad.GetCanvas().GetAbsWNDC()
+        padheight = cpad.GetWh() * cpad.GetAbsHNDC()
+        xscale = 700.0 / canvaswidth
+        yscale = canvaswidth / canvasheight
+        yscale *= canvasheight / padheight
+        yscale *= 700.0 / canvaswidth
+        height = self.GetY2() - self.GetY1()
+        self.SetX1(self.GetX2() - ((self.GetX2() - self.GetX1()) * xscale))
+        self.SetY1(self.GetY2() - ((self.GetY2() - self.GetY1()) * yscale))
         super(Legend, self).Draw(option + "SAME")
 
     def GetNRows(self):
@@ -112,18 +125,21 @@ class Legend(MethodProxy, ROOT.TLegend):
             if self.GetNColumns() > 1 and i % self.GetNColumns() == 1:
                 lastcolmaxtitlewidth = max(lastcolmaxtitlewidth, title.GetXsize())
         x2 = 0.925 + self._xshift
-        x1 = max(
-            x2 - self._maxwidth,
-            x2
-            - max(
-                self.GetNColumns()
-                * (1.5 * (self.GetTextSize() / 700.0) + maxtitlewidth),
-                self.GetMargin() / 1.6,
-            ),
+        x1 = (
+            max(
+                x2 - self._maxwidth,
+                x2
+                - max(
+                    self.GetNColumns()
+                    * (1.5 * (self.GetTextSize() / 700.0) + maxtitlewidth),
+                    self.GetMargin() / 1.6,
+                ),
+            )
+            + self._xshift
         )
-        y2 = 0.935 + self._yshift
-        y1 = y2 - (1.2 * self.GetNRows() * maxtitleheight)
-        self.DeclareProperties(x1=x1, x2=x2, y1=y1, y2=y2)
+        y2 = 0.955 + self._yshift
+        y1 = y2 - (1.2 * self.GetNRows() * maxtitleheight) + self._yshift
+        self.DeclareProperties(x1=x1, y1=y1, x2=x2, y2=y2)
 
     def SetXShift(self, shift):
         self._xshift = shift
