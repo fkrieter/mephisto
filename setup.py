9dc0ee6c481e9bbf029ad8a406bc14dc8f5e1ada
fkrieter <46888282+fkrieter@users.noreply.github.com>
2019-11-08 10:48:00 +0100

    set upper limit for python version

--- setup.py
+++ setup.py
@@ -24,5 +24,6 @@ setuptools.setup(
         "Programming Language :: Python :: 2.7",
     ],
     scripts=[],
-    install_requires=["numpy<=1.16.5a0", "root_numpy<=4.8.0a0", "scipy<=1.2.2a0"],
+    python_requires="<=2.7",
+    install_requires=["root_numpy", "scipy"],
 )
