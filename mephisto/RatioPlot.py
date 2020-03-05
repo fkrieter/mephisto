20caeeb66383e1dc9cfa6664efa7ddd1aaa3b809
fkrieter <46888282+fkrieter@users.noreply.github.com>
2020-03-05 14:17:43 +0100

    implement support for RatioPlots with more than one numerator

--- mephisto/RatioPlot.py
+++ mephisto/RatioPlot.py
@@ -14,21 +14,16 @@ from Helpers import DissectProperties, MergeDicts, IsInherited
 
 
 def ExtendProperties(cls):
-    # Add properties to configure the _baseline member histogram of RatioPlots and add
-    # new properties and methods manually since RatioPlot does not inherit from
-    # MethodProxy directly.
-    cls._properties += ["baseline{}".format(p) for p in cls._properties]  # append!
-    for setter in [
-        f for f in dir(cls) if f.startswith("Set") and callable(getattr(cls, f))
-    ]:
-        if not IsInherited(cls, setter):
-            cls._properties.append(setter[3:].lower())
-            cls._methods.append(setter)
+    # Add properties to configure the _baseline histogram of RatioPlots and add new
+    # properties and methods manually since RatioPlot does not inherit from MethodProxy
+    # directly.
+    cls._properties += ["baseline{}".format(p) for p in Histo1D._properties]  # append!
     return cls
 
 
 @ExtendProperties
-class RatioPlot(Histo1D):
+@PreloadProperties
+class RatioPlot(MethodProxy):
     r"""Class for comparing 1-dimensional histograms.
 
     +-------------------------------------------------------------------------------+
@@ -44,6 +39,9 @@ class RatioPlot(Histo1D):
     front of the property name.
     """
 
+    # Properties not meant to be changed via keyword arguments:
+    _ignore_properties = ["name"]
+
     def __init__(self, numerator, denominator=0, **kwargs):
         r"""Initialize a ratio plot for 1-dimensional histograms.
 
@@ -58,24 +56,28 @@ class RatioPlot(Histo1D):
 
         :param \**kwargs: :class:`.RatioPlot` properties
         """
-        self._loadTemplates()
+        MethodProxy.__init__(self)
+        self._name = uuid4().hex[:8]
         self._drawarrows = False
         self._drawbenchmarklines = False
-        if isinstance(numerator, list):
-            # TODO: Compute and plot multiple at once
-            raise NotImplementedError
-        assert numerator.InheritsFrom("TH1")
+        if not isinstance(numerator, list):
+            numerator = [numerator]
         kwargs.setdefault("template", "common")
         if denominator.InheritsFrom("TH1"):
-            super(RatioPlot, self).__init__(
-                "{}_RatioPlotNumerator".format(numerator.GetName()), numerator
-            )
             self._baseline = Histo1D(
-                "{}_RatioPlotDenominator".format(numerator.GetName()), denominator
+                "{}_RatioPlotDenominator".format(denominator.GetName()), denominator
             )
-            self.Divide(
-                self, self._baseline, 1.0, 1.0, kwargs.get("divideoption", "pois")
-            )
+            self._numeratorhistos = []
+            for i, histo in enumerate(numerator, start=1):
+                assert histo.InheritsFrom("TH1")
+                numname = histo.GetName()
+                numerator = Histo1D(
+                    "{}_RatioPlotNumerator{}".format(denominator.GetName(), i), histo
+                )
+                numerator.Divide(
+                    histo, self._baseline, 1.0, 1.0, kwargs.get("divideoption", "pois")
+                )
+                self._numeratorhistos.append(numerator)
             for bn in range(self._baseline.GetNbinsX() + 2):
                 try:
                     self._baseline.SetBinError(
@@ -94,27 +96,84 @@ class RatioPlot(Histo1D):
             raise NotImplementedError
         else:
             raise TypeError
+        self._ymin = None
+        self._ymax = None
+        self._numeratordrawoption = ""
         self.DeclareProperties(**kwargs)
 
-    def Draw(self, drawoption=None):
+    def Draw(self, option=None):
         # Draw the baseline, its errorband, the numerator histograms and 'out-of-range'
-        # arrows.
+        # arrows. The specified draw option will be used for all numerator histograms.
         self._baseline.Draw(self._baseline.GetDrawOption() + "SAME")  # Histo1D.Draw
-        self._baseline.DrawCopy("HIST SAME", "_{}".format(uuid4().hex[:8]))
         if self._drawbenchmarklines:
             self.DrawBenchmarkLines()
-        super(RatioPlot, self).Draw(drawoption)
+        self.SetDrawOption(option.upper().replace("SAME", ""))
+        for numerator in self._numeratorhistos:
+            numerator.Draw(numerator.GetDrawOption() + "SAME")
         if self._drawarrows:
             self.DrawArrows()
 
     def DeclareProperty(self, property, args):
-        # Properties starting with "baseline" will be applied to the self._baseline
-        # histogram.
+        # Properties starting with "baseline" will be applied to the denominator
+        # histogram
         property = property.lower()
         if property.startswith("baseline"):
             self._baseline.DeclareProperty(property[8:], args)
         else:
+            # for numerator in self._numeratorhistos:
+            #     numerator.DeclareProperty(property, args)
             super(RatioPlot, self).DeclareProperty(property, args)
+        pass
+
+    def SetYMin(self, value):
+        r"""Set the minimal value of the y-axis.
+
+        :param value: minimal value of the y-axis
+        :type value: ``float``
+        """
+        self._ymin = value
+
+    def GetYMin(self):
+        r"""Return the minimal value of the y-axis.
+
+        :returntype: ``float``
+        """
+        return self._ymin
+
+    def SetYMax(self, value):
+        r"""Set the maximal value of the y-axis.
+
+        :param value: maximal value of the y-axis
+        :type value: ``float``
+        """
+        self._ymax = value
+
+    def GetYMax(self):
+        r"""Return the maximal value of the y-axis.
+
+        :returntype: ``float``
+        """
+        return self._ymax
+
+    def BuildFrame(self, **kwargs):
+        # Compute optimal x- and y-axis ranges.
+        frame = {}
+        for histo in self._numeratorhistos:
+            if not frame:
+                frame = histo.BuildFrame(**kwargs)
+            else:
+                for key, func in [
+                    ("xmin", min),
+                    ("ymin", min),
+                    ("xmax", max),
+                    ("ymax", max),
+                ]:
+                    frame[key] = func(frame[key], histo.BuildFrame(**kwargs)[key])
+        if self._ymin is not None:
+            frame["ymin"] = self._ymin
+        if self._ymax is not None:
+            frame["ymax"] = self._ymax
+        return frame
 
     def DrawArrows(self, **kwargs):
         # Draw the 'out-of-range' arrows for ratio value outside the given y-axis range.
@@ -124,28 +183,26 @@ class RatioPlot(Histo1D):
         ymax = currentpad.GetUymax()
         ymin = currentpad.GetUymin()
         self._arrows = []
-        for bn in range(1, self.GetNbinsX() + 1, 1):
-            if self.GetBinContent(bn) == 0.0:
+        for bn in range(1, self._baseline.GetNbinsX() + 1, 1):
+            bnclist = [h.GetBinContent(bn) for h in self._numeratorhistos]
+            if all([bnc == 0 for bnc in bnclist]):
                 continue
-            sign = -1 if self.GetBinContent(bn) > ymax else 1
-            if self.GetBinContent(bn) == 0.0:
-                continue
-            if self.GetBinContent(bn) > ymax:
+            if max(bnclist) > ymax:
                 self._arrows.append(
                     Arrow(
-                        self.GetXaxis().GetBinCenter(bn),
+                        self._baseline.GetXaxis().GetBinCenter(bn),
                         ymax - (ymax - ymin) * 0.03,
-                        self.GetXaxis().GetBinCenter(bn),
+                        self._baseline.GetXaxis().GetBinCenter(bn),
                         ymax - (ymax - ymin) * 0.15,
                         arrowsize=0.01,
                     )
                 )
-            elif self.GetBinContent(bn) < ymin:
+            elif min(bnclist) < ymin:
                 self._arrows.append(
                     Arrow(
-                        self.GetXaxis().GetBinCenter(bn),
+                        self._baseline.GetXaxis().GetBinCenter(bn),
                         ymin + (ymax - ymin) * 0.03,
-                        self.GetXaxis().GetBinCenter(bn),
+                        self._baseline.GetXaxis().GetBinCenter(bn),
                         ymin + (ymax - ymin) * 0.17,
                         arrowsize=0.01,
                     )
@@ -164,7 +221,7 @@ class RatioPlot(Histo1D):
         ymax = currentpad.GetUymax()
         self._benchmarklines = []
         for bm in [i * 0.1 for i in range(-5, 41, 5)]:
-            if bm < ymin:
+            if bm < ymin or bm == 1:
                 continue
             if bm > ymax:
                 break
@@ -173,6 +230,44 @@ class RatioPlot(Histo1D):
             )
         for line in self._benchmarklines:
             line.Draw()
+
+    def SetName(self, name):
+        r"""Set the name of object.
+
+        :param name: name of the object
+        :type name: ``str``
+        """
+        self._name = name
+
+    def GetName(self):
+        r"""Return the name of the object.
+
+        :returntype: ``str``
+        """
+        return self._name
+
+    def InheritsFrom(self, classname):
+        # Dummy function (SensitivityScan does not inherit from any ROOT function)
+        return False
+
+    def SetDrawOption(self, option):
+        r"""Define the draw option for all numerator histograms.
+
+        :param option: draw option (see :class:`ROOT.THistPainter`
+            `class reference <https://root.cern/doc/master/classTHistPainter.html>`_)
+        :type option: ``str``
+        """
+        if option:
+            self._numeratordrawoption = option
+            for numerator in self._numeratorhistos:
+                numerator.SetDrawOption(self._numeratordrawoption)
+
+    def GetDrawOption(self):
+        r"""Return the draw option defined for all numerator histograms.
+
+        :returntype: ``str``
+        """
+        return self._numeratordrawoption
 
     def SetDrawArrows(self, boolean):
         r"""Define whether the arrows should be drawn.
@@ -213,17 +308,23 @@ if __name__ == "__main__":
 
     filename = "../data/ds_data18.root"
 
-    h1 = Histo1D("test1", "", 20, 0.0, 400.0)
-    h2 = Histo1D("test2", "", 20, 0.0, 400.0)
+    RatioPlot.PrintAvailableProperties()
+
+    h1 = Histo1D("test1", "test1", 20, 0.0, 400.0)
+    h2 = Histo1D("test2", "test2", 20, 0.0, 400.0)
+    h3 = Histo1D("test3", "test3", 20, 0.0, 400.0)
     h1.Fill(filename, tree="DirectStau", varexp="MET", cuts="tau1Pt>650")
     h2.Fill(filename, tree="DirectStau", varexp="MET", cuts="tau1Pt>620")
+    h3.Fill(filename, tree="DirectStau", varexp="MET", cuts="tau1Pt>700")
 
     h1.DeclareProperty("template", "background")
-    h2.DeclareProperty("template", "data")
-    rp = RatioPlot(h2, h1)
+    h2.DeclareProperties(template="data", markercolor="#a7126b", linecolor="#a7126b")
+    h3.DeclareProperties(template="signal", linecolor="#17a242")
+    rp = RatioPlot([h2, h3], h1)
 
     p1 = Plot(npads=2)
     p1.Register(h1, 0)
     p1.Register(h2, 0, xunits="GeV")
-    p1.Register(rp, 1, logy=False, ymin=0.5, ymax=2, ytitle="Data / Bkg.")
+    p1.Register(h3, 0)
+    p1.Register(rp, 1, logy=False, ymin=0.5, ymax=2.0, ytitle="Data / Bkg.")
     p1.Print("ratioplot_test.pdf")
